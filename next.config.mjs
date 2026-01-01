36e2b461b3182e002f7aadaaf0435f8fe1ae2f96
Matthieu Blanc <blanc.matthieu@gmail.com>
2026-01-01 16:39:06 +0100

    Add security headers

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,49 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    async headers() {
+        return [
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "X-DNS-Prefetch-Control",
+                        value: "on",
+                    },
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=63072000; includeSubDomains; preload",
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "SAMEORIGIN",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "origin-when-cross-origin",
+                    },
+                    {
+                        key: "Content-Security-Policy",
+                        value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-eval' 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' blob: data: https://storage.googleapis.com https://lh3.googleusercontent.com;
+              media-src 'self' blob: https://storage.googleapis.com;
+              connect-src 'self' https://*.googleapis.com https://*.google.com;
+              frame-ancestors 'none';
+            `
+                            .replace(/\s{2,}/g, " ")
+                            .trim(),
+                    },
+                ],
+            },
+        ];
+    },
 };
 
 export default nextConfig;
